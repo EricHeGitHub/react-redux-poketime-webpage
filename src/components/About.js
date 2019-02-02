db44c2ce968282b1e68c5573c2450ee281d1a341
Jiawei He <nmghejiawei@hotmail.com>
2019-02-02 19:24:15 +1100

    Upload repository

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow.js'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam sit obcaecati eaque quam rem similique, sunt, cum distinctio fugiat accusantium adipisci. Itaque molestias ipsa, explicabo quod et omnis fugiat.</p>
+        </div>
+    )
+}
+
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('./about')
+    // },2000)
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ullam sit obcaecati eaque quam rem similique, sunt, cum distinctio fugiat accusantium adipisci. Itaque molestias ipsa, explicabo quod et omnis fugiat.</p>
+        </div>
+    )
+}
+
+export default Contact
